0419eb50c249fb93fcf206a9d685748a35a82231
IsaiahW <i.m.waddingtonsafem@gmail.com>
2023-11-22 18:50:13 -0600

    changing to my own

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,34 +2,39 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Rock Band T-Shirt',
     price: 14.99,
     stock: 14,
     category_id: 1,
+    tags: ['Rock Music', 'Clothing', 'Fashion']
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Wireless Earbuds',
     price: 90.0,
     stock: 25,
     category_id: 5,
+    tags: ['Pop Music', 'Electronics']
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'Musician's Fedora Hat',
     price: 22.99,
     stock: 12,
     category_id: 4,
+    tags: ['Accessories', 'Fashion']
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    product_name: 'Vinyl Record Collection',
     price: 12.99,
     stock: 50,
     category_id: 3,
+    tags: ['Vinyl', 'Music']
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Acoustic Guitar',
     price: 29.99,
     stock: 22,
     category_id: 2,
+    tags: ['Music', 'Instruments']
   },
 ];
 
